ab82a5e0fc46ba31d65cd2fcc619adc47ca96bef
Bongani Luwemba <bongtheman@outlook.com>
2026-01-31 06:27:54 +0000

    feat: implement authentication and user management with better-auth integration. added the auth layouts

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,9 @@
-import "@/styles/globals.css";
+import "@/styles/globals.css"
 
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
-import { TRPCReactProvider } from "@/trpc/react";
+import { Providers } from "@/app/(main)/providers";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -22,7 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geist.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+                    <Providers>
+                       {children}
+                    </Providers>
+
       </body>
     </html>
   );
